7a3050b608da6b1bca2591039f68fe83e5d0fdef
yvsKaan <yavas.kaann@gmail.com>
2021-08-11 11:06:51 +0300

    ntp client changed

--- ntpclient.py
+++ ntpclient.py
@@ -1,28 +1,29 @@
 import ntplib
-from time import ctime
-from datetime import datetime, timezone
-
-def get_ntp_server():
-    try:
-        client = ntplib.NTPClient()
-        host_address = input('Host address input:')
-        port_address = input('Port address input:')
-        response = client.request(host=host_address, port=port_address)
-
-        if response:
-            print('You are connected to NTP server')
-            print(f"Your host: {host_address}")
-            print(f"Your port: {port_address}")
-            print(f"Today: {ctime(response.orig_time)}")
-            print(f"Client time of request: {datetime.fromtimestamp(response.orig_time, timezone.utc)}")
-            print(f"Server responded with: {datetime.fromtimestamp(response.tx_time, timezone.utc)}")
-
-        else: 
-            print("You are not connected to NTP server")
-    except Exception as e:
-        print(e)
+import socket
 
 
-get_ntp_server()
+client = ntplib.NTPClient()
+host_address = input('Host address input:')
+port_address = input('Port address input:')
+serverAddress = (host_address, int(port_address))
+bufferSize = 1024
 
-   
+try:
+    UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+    while True:
+        clientMessage = input("Enter your message:")
+        if clientMessage == "exit":
+            break
+        bytesToSend = str.encode(clientMessage+ "+1")
+
+        UDPServerSocket.sendto(bytesToSend, serverAddress)
+
+        serverMessage = UDPServerSocket.recvfrom(bufferSize)
+
+        msg = "Server message:  {}".format(serverMessage[0].decode())
+        
+        print(msg)
+
+except Exception as e:
+    print(e)
